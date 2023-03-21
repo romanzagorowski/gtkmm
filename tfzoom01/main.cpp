1487f66feb8ba7113511f9176624cd80fe0857ba
Roman Zagorowski <roman.zagorowski@hotmail.com>
2023-03-21 18:59:38 +0100

    CMake based skeleton for tfzoom01 added

---
+++ tfzoom01/main.cpp
@@ -0,0 +1,13 @@
+#include <gtkmm/application.h>
+#include <gtkmm/window.h>
+
+int main(int argc, char* argv[])
+{
+	auto app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.tfzoom01");
+
+	Gtk::Window win;
+	win.set_default_size(800, 600);
+	win.show_all();
+
+	return app->run(win);
+}
