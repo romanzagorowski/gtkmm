f1851b44114434f8960c62046934cbce9e8742d8
Roman Zagorowski <roman.zagorowski@hotmail.com>
2023-03-21 21:06:40 +0100

    Some flexibility added

---
+++ tfzoom01/f1.cpp
@@ -0,0 +1,7 @@
+#include "mt4hst.h"
+#include "stdio.h"
+
+void f1()
+{
+    printf("%s\n", __PRETTY_FUNCTION__);
+}
--- tfzoom01/main.cpp
+++ tfzoom01/main.cpp
@@ -1,13 +1,8 @@
-#include <gtkmm/application.h>
-#include <gtkmm/window.h>
+void f1();
+void f0(int argc, char* argv[]);
 
 int main(int argc, char* argv[])
 {
-	auto app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.tfzoom01");
-
-	Gtk::Window win;
-	win.set_default_size(800, 600);
-	win.show_all();
-
-	return app->run(win);
+	//f0(argc, argv);
+	f1();
 }
